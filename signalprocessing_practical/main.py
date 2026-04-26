bd1eb8ddf2c7e0f60c78183df0eab197ba8af84b
unknown <y.s.prokofiev@student.khai.edu>
2026-04-26 15:13:29 +0300

    practical 3

--- signalprocessing_practical/main.py
+++ signalprocessing_practical/main.py
@@ -4,8 +4,64 @@ from math import gcd
 from scipy.signal import resample_poly, butter, sosfiltfilt
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.restoration import denoise_wavelet, denoise_invariant, denoise_tv_chambolle, denoise_bilateral
+import pywt
 
 SAMPLE_RATE = 44100
+NAME_ORIGINAL_WAV = "./Sounds/Sound_44100[Hz]_2[byte].wav"
+
+def wavelet_denoiser(signal, level, mode, wavelet):
+    coeffs = pywt.wavedec(signal, wavelet, level=level)
+    sigma = np.median(np.abs(coeffs[-1])) / 0.6745
+    threshold = sigma * np.sqrt(2 * np.log(signal.size))
+    denoised_coeffs = [coeffs[0]] + [
+        pywt.threshold(c, threshold, mode=mode) for c in coeffs[1:]
+    ]
+    denoised_signal = pywt.waverec(denoised_coeffs, wavelet)
+    return denoised_signal[:len(signal)]
+
+def invarince_denoiser(image, **kwargs):
+    return denoise_wavelet(image, sigma=0.5, wavelet='db4', mode='soft')
+
+def plot_signal(time, original, filtered, title, filename):
+    plt.figure(figsize=(10, 6))
+    plt.plot(time, original, 'b-', label='Original Clean Signal')
+    plt.plot(time, filtered, 'g-', linewidth=2, label=title)
+    plt.title(title)
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(filename)
+    plt.show()
+
+def sound_filter():
+    data, fs_original = sf.read(NAME_ORIGINAL_WAV)
+    time = np.arange(len(data)) / fs_original
+    data_2d = data.reshape(1, -1)
+
+    invariance = denoise_invariant(data_2d, denoise_function=invarince_denoiser).flatten()
+    total_variation = denoise_tv_chambolle(data_2d, weight=0.1, channel_axis=None).flatten()
+    bilateral = denoise_bilateral(data_2d, sigma_color=0.05, sigma_spatial=15, channel_axis=None).flatten()
+    wavelet = wavelet_denoiser(data, level=5, mode='soft', wavelet='db4')
+
+    sf.write("./Sounds/Filtered_Invariance.wav", invariance, SAMPLE_RATE)
+    sf.write("./Sounds/Filtered_Total_Variation.wav", total_variation, SAMPLE_RATE)
+    sf.write("./Sounds/Filtered_Bilateral.wav", bilateral, SAMPLE_RATE)
+    sf.write("./Sounds/Filtered_Wavelet.wav", wavelet, SAMPLE_RATE)
+    plot_signal(time, data, invariance, "J-Invariance", "./Sounds/invariance.png")
+    plot_signal(time, data, total_variation, "Total Variation", "./Sounds/total_variation.png")
+    plot_signal(time, data, bilateral, "Bilateral", "./Sounds/bilateral.png")
+    plot_signal(time, data, wavelet, "Wavelet", "./Sounds/wavelet.png")
+
+
+if __name__ == "__main__":
+    '''recognizer = srec.Recognizer()
+    microphone = srec.Microphone(device_index=1, sample_rate=SAMPLE_RATE)
+    sound_recoder(recognizer, microphone)'''
+    sound_filter()
+
+'''SAMPLE_RATE = 44100
 SAMPLE_WIDTH = 2
 DTYPE = np.int16
 NAME_ORIGINAL_WAV = f"./Sounds/Sound_{SAMPLE_RATE}[Hz]_{SAMPLE_WIDTH}[byte].wav"
@@ -88,9 +144,5 @@ def sound_recoder(rec, mic):
     plt.legend()
     plt.tight_layout()
     plt.savefig("./Sounds/signal.png", dpi=600)
-    plt.show()
+    plt.show()'''
 
-if __name__ == "__main__":
-    recognizer = srec.Recognizer()
-    microphone = srec.Microphone(device_index=1, sample_rate=SAMPLE_RATE)
-    sound_recoder(recognizer, microphone)
